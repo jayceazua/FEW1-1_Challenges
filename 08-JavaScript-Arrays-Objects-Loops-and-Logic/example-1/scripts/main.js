ffbf9850667597381ef9185ec33ef53488fb36ca
Jayce Azua <jayce.azua@gmail.com>
2018-09-24 14:51:19 -0700

    ...

--- 08-JavaScript-Arrays-Objects-Loops-and-Logic/example-1/scripts/main.js
+++ 08-JavaScript-Arrays-Objects-Loops-and-Logic/example-1/scripts/main.js
@@ -1,31 +1,47 @@
 // Your scripts here
 const container = document.getElementById('container');
 
-let str = '';
-
-for (var i = 0; i < data.length; i += 1) {
-  const alien = data[i];
-  // console.log(alien);
-  str += `<div class="">${alien.name}</div>`;
+let displayAliens = () => {
+    let str = ''
+    for (var i = 0; i < data.length; i += 1) {
+      const alien = data[i];
+      const {name, image, power, rof, health} = alien;
+      console.log(alien);
+      str += `
+      <div class="alien-card">
+        <img src="${image}" width="32" height="32">
+        <h1>${name}</h1>
+        <div><span>Power: </span>${power}</div>
+        <div><span>R.O.F.: </span>${rof}</div>
+        <div><span>Health: </span>${health}</div>
+      </div>
+        `;
+    }
+    return str
 }
 
-container.innerHTML = str;
+
+container.innerHTML = displayAliens();
 
 /*
 Challenges: Your goal is to display a catalog of Aliens.
 The aliens are stored in an array named: data.
 Each element in the array is an object with properties that
 describe an alien.
+
 1. Explore the Array and contained Objects and identify the
 properties. Print each alien to the console.
+
 2. The default code prints the name of each Alien. In a div.
 Add the other properties within the div. You'll want to add
 some more markup. Use the example markup below for inspiration.
+
 3. Generating all of the code within the for loop works. But,
 the system could work better. A function would be an improvement.
 Make a function that generates the HTML block
   - It should take in an obj as a parameter
   - It should return an HTML string
+
 4. Add a style sheet (you can copy your styles from a previous
 example)
 <!--
