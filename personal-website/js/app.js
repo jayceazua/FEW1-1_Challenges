569457236b0c5696c4ee6d1d75166fb5ae4884d9
Jayce Azua <jayce.azua@gmail.com>
2018-10-12 13:04:05 -0700

    finishing...

--- personal-website/js/app.js
+++ personal-website/js/app.js
@@ -1,1 +1,25 @@
-console.log('connected!');
+// This function is for dynamic change of the body
+const portClick = document.getElementById('portClick');
+const aboutClick = document.getElementById('aboutClick');
+
+portClick.onclick = (event) => {
+    openCity(event, 'Portfolio')
+}
+
+aboutClick.onclick = (e) => {
+    openCity(event, 'About')
+}
+
+function openCity(evt, elem) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(elem).style.display = "block";
+    evt.currentTarget.className += " active";
+}
